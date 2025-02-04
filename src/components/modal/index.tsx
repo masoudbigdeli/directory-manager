a39a734d4691e228f79c9209c65eff679a221a86
Shaberz <85541637+Shaberz@users.noreply.github.com>
2025-02-04 16:52:57 +0330

    directory with cheat

---
+++ src/components/modal/index.tsx
@@ -0,0 +1,14 @@
+import { ModalBackDrop, ModalWrapper } from "../../styles/pages/home";
+
+const Modal = () => {
+    return (
+        <>
+        <ModalBackDrop>
+        </ModalBackDrop>
+        <ModalWrapper>JAFARE ALI</ModalWrapper>
+        </>
+    )
+};
+
+
+export default Modal
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,26 +1,43 @@
 import HomeWrapper,{HeadersWrapper, BodyWrapper, DirWrapper, DirNameWrapper} from "../../styles/pages/home";
 import FolderIcon from "../../components/icons/folder-icon";
+import Modal from "../../components/modal";
+import {  useCallback, useState } from "react";
+import DirectoryRenderer from "../../components/directory-renderer";
+export interface directoryManager {
+    id : string;
+    name: string;
+    children: Array<directoryManager>;
+    layerIndex:number;
+
+}
 export default function Home() {
+    const [ treeObject, setTreeObject ] = useState<directoryManager | null>(null);
+    const handleOnEnter = useCallback((object:directoryManager) => {
+        setTreeObject(object)
+    },[]);
+
+
+
+
   return (
     <HomeWrapper>
         <HeadersWrapper>
-            icon
+            {!treeObject ? <button onClick={(e) => {
+                setTreeObject({
+                    id:crypto.randomUUID(),
+                    name:e.currentTarget.value,
+                    children:[],
+                    layerIndex:0
+                })
+            }}>ADD</button> :null}
         </HeadersWrapper>
         <BodyWrapper>
-            <DirWrapper>Folder<div>dd</div></DirWrapper>
-            <DirWrapper>Folder</DirWrapper>
-            <DirWrapper>Folder</DirWrapper>
-            <DirWrapper>Folder</DirWrapper>
-            <DirWrapper>Folder</DirWrapper>
-            <DirWrapper>
-
-                <DirNameWrapper>
-                    <div className="icon" style={{width:'1.5rem'}}>
-                    <FolderIcon/>
-                    </div>
-                    <div className='title'>asghar ba pedarash be khane raft chera nagereft??</div>
-                </DirNameWrapper>
-            </DirWrapper>
+        {treeObject && (
+            <DirectoryRenderer 
+                directory={treeObject}
+                onEnterHandler={handleOnEnter} 
+            />
+            )}
         </BodyWrapper>
     </HomeWrapper>
   );
