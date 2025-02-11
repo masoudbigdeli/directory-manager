b5767b90c4e73485c8d10d80b4b9cc04bb3195f9
Shaberz <85541637+Shaberz@users.noreply.github.com>
2025-02-11 12:21:14 +0330

    finished add file with bug

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,54 +1,50 @@
-import HomeWrapper,{ HeadersWrapper, BodyWrapper, StatusButtons } from "../../styles/pages/home";
-import {  useCallback, useState } from "react";
+import HomeWrapper, {
+  HeadersWrapper,
+  BodyWrapper,
+  StatusButtons,
+} from "../../styles/pages/home";
+import { useCallback, useState } from "react";
 import DirectoryRenderer from "../../components/directory-renderer";
-import PlusIcon from "../../components/icons/plus-icon";
-export interface directoryManager {
-    id : string;
-    name: string;
-    children: Array<directoryManager>;
-    layerIndex:number;
-    isExtended:boolean
-
-}
+import PlusIcon from "../../components/icons/add-directory-icon";
+import { DirectoryManager } from "../../components/directory-renderer";
 export default function Home() {
-    const [ treeObject, setTreeObject ] = useState<directoryManager | null>(null);
-    const handleOnEnter = useCallback((object:directoryManager) => {
-        if (object.name === "__REMOVE__") {
-            setTreeObject(null);
-          } else {
-            setTreeObject(object);
-          }
-    },[]);
-
-
-
+  const [treeObject, setTreeObject] = useState<DirectoryManager | null>(null);
+  const handleOnEnter = useCallback((object: DirectoryManager) => {
+    if (object.name === "__REMOVE__") {
+      setTreeObject(null);
+    } else {
+      setTreeObject(object);
+    }
+  }, []);
 
   return (
     <HomeWrapper>
-        <HeadersWrapper>
-            <span>Directory Manager</span>
-            {!treeObject 
-            ? <StatusButtons onClick={(e) => {
-                        setTreeObject({
-                            id:crypto.randomUUID(),
-                            name:e.currentTarget.value,
-                            children:[],
-                            layerIndex:0,
-                            isExtended:true
-                        })
-                    }}>
-                <PlusIcon/>
-            </StatusButtons> 
-            :null}
-        </HeadersWrapper>
-        <BodyWrapper>
+      <HeadersWrapper>
+        <span>Directory Manager</span>
+        {!treeObject ? (
+          <StatusButtons
+            onClick={(e) => {
+              setTreeObject({
+                id: crypto.randomUUID(),
+                name: e.currentTarget.value,
+                children: [],
+                layerIndex: 0,
+                isExtended: true,
+              });
+            }}
+          >
+            <PlusIcon />
+          </StatusButtons>
+        ) : null}
+      </HeadersWrapper>
+      <BodyWrapper>
         {treeObject && (
-            <DirectoryRenderer 
-                directory={treeObject}
-                onEnterHandler={handleOnEnter} 
-            />
-            )}
-        </BodyWrapper>
+          <DirectoryRenderer
+            directoryOrFile={treeObject}
+            onEnterHandler={handleOnEnter}
+          />
+        )}
+      </BodyWrapper>
     </HomeWrapper>
-  )
+  );
 }
